849affb2efd97f23a521e43cd5f9f702dc08c88a
JurabekAbdusattarov <JorabekAbdusattarov95@gmail.com>
2021-11-16 14:18:20 +0500

    Model ichi toldirildi

--- src/main/java/com/example/agroinapp/Model/Employee.java
+++ src/main/java/com/example/agroinapp/Model/Employee.java
@@ -1,4 +1,30 @@
 package com.example.agroinapp.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String middleName;
+
+    private String username;
+
+    private String phoneNumber;
 }
