fd059afc2348ab9f974b6005783b0ff9d1c6026c
JurabekAbdusattarov <JorabekAbdusattarov95@gmail.com>
2021-11-16 13:26:48 +0500

    Initial commit

---
+++ src/main/java/com/example/agroinapp/AgroinAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.agroinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroinAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agroinapp/AgroinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agroinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroinAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
